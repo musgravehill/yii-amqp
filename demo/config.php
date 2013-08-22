dfeec25d42ebb8b8756c37bad5bbb9a93e358d3b
musgravehill <musgravehill@gmail.com>
2013-08-22 17:08:34 +0400

    Update config.php

--- demo/config.php
+++ demo/config.php
@@ -5,7 +5,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 define('HOST', 'localhost');
 define('PORT', 5672);
 define('USER', 'guest');
-define('PASS', 'mdeed');
+define('PASS', 'guest');
 define('VHOST', '/');
 
 //If this is enabled you can see AMQP output on the CLI
